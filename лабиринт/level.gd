7636d67828143426911637553aaead82e993c079
nikta9922 <kridegor208@gmail.com>
2025-09-06 13:35:05 +0700

    strat,finish,stamina,time.

---
+++ лабиринт/level.gd
@@ -0,0 +1,22 @@
+extends Node2D
+var game_start=false
+var time=300
+func _on_zone_finish_body_entered(body: Node2D) -> void:
+	if body.name=="Plaer":
+		$CanvasLayer/MenuBar.visible=true
+
+
+func _on_button_pressed() -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_zone_started_body_exited(body: Node2D) -> void:
+	game_start=true
+func _process(delta: float) -> void:
+	if game_start==true:
+		time-=delta
+		var min=int(time)/60
+		var sec=int(time)%60
+		print(min,":",sec)
+		$CanvasLayer/Label.text=str(min,":",sec)
+		
--- лабиринт/plaer.gd
+++ лабиринт/plaer.gd
@@ -3,21 +3,22 @@ var speed=50
 var speed_shift=100
 var Max_Stamina=100
 var stamina=100
-var Min_stamina=0
 func _physics_process(delta: float) -> void:
 	var Input_vector=Vector2(0,0)
 	Input_vector.x=Input.get_action_strength("right")-Input.get_action_strength("left")
 	Input_vector.y=Input.get_action_strength("down")-Input.get_action_strength("up")
 	velocity=Input_vector*speed
-	if Input.is_action_pressed("Stamina"):
+	$ProgressBar.value=stamina
+	if stamina>Max_Stamina:
+		stamina=Max_Stamina
+	if Input.is_action_pressed("Stamina") and stamina>0:
 		stamina-=1
 		speed=speed_shift
-	else: 
-		Min_stamina<0
-		speed_shift=0
+	else:
 		stamina+=0.5
 		speed=50
-		
+	if stamina<2:
+		speed=50
 	print(stamina)
 	move_and_slide()
 	if Input_vector==Vector2(0,0):
