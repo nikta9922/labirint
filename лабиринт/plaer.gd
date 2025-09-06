624c52aaa5a7cad0466df78b195e55b641ea6fcb
nikta9922 <kridegor208@gmail.com>
2025-09-06 12:36:14 +0700

    1223

--- лабиринт/plaer.gd
+++ лабиринт/plaer.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
-var speed=100
-var speed_shift=150
+var speed=50
+var speed_shift=100
 var Max_Stamina=100
 var stamina=100
+var Min_stamina=0
 func _physics_process(delta: float) -> void:
 	var Input_vector=Vector2(0,0)
 	Input_vector.x=Input.get_action_strength("right")-Input.get_action_strength("left")
@@ -12,8 +13,11 @@ func _physics_process(delta: float) -> void:
 		stamina-=1
 		speed=speed_shift
 	else: 
+		Min_stamina<0
+		speed_shift=0
 		stamina+=0.5
-		speed=100
+		speed=50
+		
 	print(stamina)
 	move_and_slide()
 	if Input_vector==Vector2(0,0):
